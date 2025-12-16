76a7065aa53196872fd6cd6be724451dd40ca1a9
WesleyAraujoFernandes <wesley.a.fernandes@gmail.com>
2025-12-16 14:08:26 -0300

    user storage service complete

--- src/app/basic/basic-services/user-storage.service.ts
+++ src/app/basic/basic-services/user-storage.service.ts
@@ -16,4 +16,56 @@ export class UserStorageService {
   static getUser(): any {
     return JSON.parse(localStorage.getItem(USER));
   }
+
+  static getUserId(): string {
+    const user = this.getUser();
+    if (user == null) {
+      return '';
+    }
+    return user.id;
+  }
+
+  static getUserProjectId(): string {
+    const user = this.getUser();
+    if (user == null) {
+      return '';
+    }
+    return user.projectId;
+  }
+
+  static getUserRole(): string {
+    const user = this.getUser();
+    if (user == null) {
+      return '';
+    }
+    return user.userRole;
+  }
+
+  static isAdminLoggedIn(): boolean {
+    if (this.getUserRole() == '') {
+      return false;
+    }
+    const role: string = this.getUserRole();
+    return role == 'ADMIN';
+  }
+
+  static isEmployeeLoggedIn(): boolean {
+    if (this.getUserRole() == '') {
+      return false;
+    }
+    const role: string = this.getUserRole();
+    return role == 'EMPLOYEE';
+  }
+
+  static isManagerLoggedIn(): boolean {
+    if (this.getUserRole() == '') {
+      return false;
+    }
+    const role: string = this.getUserRole();
+    return role == 'MANAGER';
+  }
+
+  static signOut(): void {
+    window.localStorage.removeItem(USER);
+  }
 }
