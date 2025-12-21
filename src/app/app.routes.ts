89d05fd69328465e36f774ddbf12d36490fa2b66
WesleyAraujoFernandes <wesley@viaapia.com.br>
2025-12-21 00:17:34 -0300

    criação das rotas para funcionários

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,4 +8,14 @@ export const routes: Routes = [
     loadChildren: () =>
       import('./admin/admin.module').then((m) => m.AdminModule),
   },
+  {
+    path: 'employee',
+    loadChildren: () =>
+      import('./employee/employee.module').then((m) => m.EmployeeModule),
+  },
+  {
+    path: 'manager',
+    loadChildren: () =>
+      import('./manager/manager.module').then((m) => m.ManagerModule),
+  },
 ];
