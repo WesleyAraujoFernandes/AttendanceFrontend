4dbc7009a149c54413b2608882b99a0692520891
WesleyAraujoFernandes <wesley@viaapia.com.br>
2025-12-21 21:45:10 -0300

    lista de atendimento

---
+++ src/app/manager/services/manager.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { UserStorageService } from '../../basic/basic-services/user-storage.service';
+
+const url = 'http://localhost:8080/';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ManagerService {
+  constructor(private http: HttpClient) {}
+
+  getEmployees() {
+    const projectId = UserStorageService.getUserProjectId();
+    return this.http.get(url + `managers/employees/${projectId}`);
+    //return this.http.get<any[]>(url + `manager/employees/${projectId}`);
+  }
+}
